c1448d6ccd1d89b9cb88f2fe5c3da0221ab26ff1
RatzGabriel <gabrielraz84@gmail.com>
2023-04-04 07:56:10 +0200

    add firestore

--- src/components/ui/ResultContainer.jsx
+++ src/components/ui/ResultContainer.jsx
@@ -3,37 +3,32 @@ import { useSelector } from 'react-redux';
 import ContainerTitle from './ContainerTitle';
 
 const ResultContainer = () => {
-	const searchValue = useSelector((store) => store.books.searchValue);
-	const allBooks = useSelector((store) => store.books.allBooks);
-	const [searchBooks, setSearchBooks] = useState([]);
+  const searchValue = useSelector((store) => store.books.searchValue);
+  const allBooks = useSelector((store) => store.books.allBooks);
+  const [searchBooks, setSearchBooks] = useState([]);
 
-	useEffect(() => {
-		if (searchValue === '') return setSearchBooks([]);
-		console.log('searchValue', searchValue);
-		console.log('allBooks', allBooks);
-		if (
-			allBooks[searchValue] != undefined &&
-			allBooks[searchValue].length > 0
-		) {
-			setSearchBooks(allBooks[searchValue]);
-		}
-	}, [searchValue, allBooks]);
-	console.log(searchBooks);
+  useEffect(() => {
+    if (searchValue === '') return setSearchBooks([]);
 
-	return (
-		searchBooks.length > 0 && (
-			<div className="w-full">
-				<ContainerTitle
-					resultContainer="resultContainer"
-					text={searchValue}
-					books={searchBooks}
-					isSearchResult
-					title={searchValue}
-					length={searchBooks.length}
-				/>
-			</div>
-		)
-	);
+    if (allBooks[searchValue] != undefined && allBooks[searchValue].length > 0) {
+      setSearchBooks(allBooks[searchValue]);
+    }
+  }, [searchValue, allBooks]);
+
+  return (
+    searchBooks.length > 0 && (
+      <div className="w-full">
+        <ContainerTitle
+          resultContainer="resultContainer"
+          text={searchValue}
+          books={searchBooks}
+          isSearchResult
+          title={searchValue}
+          length={searchBooks.length}
+        />
+      </div>
+    )
+  );
 };
 
 export default ResultContainer;
--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-	loadFavorites,
-	loadFavoritesFromStorage,
-} from '../../utils/favoritesSlice';
+import { loadFavorites } from '../../utils/favoritesSlice';
 import ContainerTitle from '../../components/ui/ContainerTitle';
 
+// const fetchFavorites = () => async (dispatch) => {
+//   const favorites = await loadFavorites();
+//   dispatch(loadFavoritesFromStorage(favorites));
+// };
+
 const Favorites = () => {
-	const dispatch = useDispatch();
-	const favorites = useSelector((store) => store.favorites.items);
+  const dispatch = useDispatch();
+  const favorites = useSelector((store) => store.favorites.items);
 
-	const load = async () => {
-		const favoriteFromLocal = await loadFavorites();
-		dispatch(loadFavoritesFromStorage(favoriteFromLocal));
-	};
+  // const load = async () => {
+  //   const favoriteFromLocal = await loadFavorites();
+  //   dispatch(loadFavoritesFromStorage(favoriteFromLocal));
+  // };
 
-	useEffect(() => {
-		load();
-	}, []);
+  useEffect(() => {
+    console.log('favorites 1', favorites);
+    dispatch(loadFavorites());
+    console.log('favorites 2', favorites);
+  }, []);
 
-	return <ContainerTitle books={favorites} title={'Favorites'} />;
+  // useEffect(() => {
+  //   load();
+  // }, []);
+
+  console.log('favorites', favorites);
+  if (!favorites || favorites == undefined || favorites.length == 0) {
+    console.log('lentgh 0', favorites);
+    return <div className="text-3xl font-bold text-center mt-20">No favorites yet</div>;
+  }
+  return <ContainerTitle books={favorites} title={'Favorites'} />;
 };
 
 export default Favorites;
